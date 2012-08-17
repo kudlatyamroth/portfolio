1847c46f4987d90123d2e8ea10458b9da11e3fc3
Kudlaty <kfuks2@o2.pl>
2012-08-17 22:26:08 +0200

    Add color widget for markitup

--- kudlaty/static_files/js/enable_editor.js
+++ kudlaty/static_files/js/enable_editor.js
@@ -35,6 +35,25 @@ $(document).ready(function() {
             {name:'Picture', key:'P', replaceWith:'<img src="[![Source:!:http://]!]" alt="[![Alternative text]!]" />' },
             {name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
             {separator:'---------------' },
+            {name:'Colors',
+                className:'colors',
+                openWith:'<span style="color: [![Color]!];">',
+                closeWith:'</span>',
+                    dropMenu: [
+                        {name:'Yellow', openWith:'<span style="color: yellow;">',      closeWith:'</span>', className:"color_yellow" },
+                        {name:'Orange', openWith:'<span style="color: orange;">',      closeWith:'</span>', className:"color_orange" },
+                        {name:'Red',    openWith:'<span style="color: red;">',         closeWith:'</span>', className:"color_red" },
+
+                        {name:'Blue',   openWith:'<span style="color: blue;">',        closeWith:'</span>', className:"color_blue" },
+                        {name:'Purple', openWith:'<span style="color: purple;">',      closeWith:'</span>', className:"color_purple" },
+                        {name:'Green',  openWith:'<span style="color: green;">',       closeWith:'</span>', className:"color_green" },
+
+                        {name:'White',  openWith:'<span style="color: white;">',       closeWith:'</span>', className:"color_white" },
+                        {name:'Gray',   openWith:'<span style="color: gray;">',        closeWith:'</span>', className:"color_gray" },
+                        {name:'Black',  openWith:'<span style="color: black;">',       closeWith:'</span>', className:"color_black" }
+                    ]
+            },
+            {separator:'---------------' },
             {name:'Clean', className:'clean', replaceWith:function(markitup) { return markitup.selection.replace(/<(.*?)>/g, "") } },
             {name:'Preview', className:'preview',  call:'preview'}
         ]
