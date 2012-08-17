2a6b553efc9bf9d36d129ca0cd3a79faf39d543b
Kudlaty <kfuks2@o2.pl>
2012-08-17 20:42:14 +0200

    Fix look of markitup and change place of display of preview

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -16,7 +16,7 @@ class TagAdmin(TranslationAdmin):
 
 class MyEditorWidget(forms.Textarea):
     class Media:
-        css = { 'all' : ( 'css/ui-lightness/jquery-ui-1.8.23.css', 'js/markitup/skins/markitup/style.css', 'js/markitup/sets/default/style.css') }
+        css = { 'all' : ( 'css/ui-lightness/jquery-ui-1.8.23.css', 'js/markitup/skins/markitup/style.css', 'js/markitup/sets/default/style.css', 'css/markitup.css') }
         js = (
             'js/jquery-1.8.0.min.js',
             'js/jquery-ui-1.8.23.min.js',
